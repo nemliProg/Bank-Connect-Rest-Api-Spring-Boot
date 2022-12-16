4edc087f511de7461b17e58f5b2ead60eb1f79de
nemliProg <younesbuzz@gmail.com>
2022-12-16 20:31:01 +0100

    add Transaction repository

---
+++ bank-connect/src/main/java/ma/bankconnect/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package ma.bankconnect.repository;
+
+import ma.bankconnect.entities.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
