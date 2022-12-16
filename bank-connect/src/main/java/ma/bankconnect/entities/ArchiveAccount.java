edb12ec40cac4c7487a9fea3a28560211cd0fbd7
nemliProg <younesbuzz@gmail.com>
2022-12-16 19:28:34 +0100

    add Archive Account Entity

---
+++ bank-connect/src/main/java/ma/bankconnect/entities/ArchiveAccount.java
@@ -0,0 +1,27 @@
+package ma.bankconnect.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArchiveAccount {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private long id;
+
+    @OneToOne
+    @JoinColumn( name = "account_id")
+    private Account account;
+
+
+    private String reason;
+}
