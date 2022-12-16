18f14fc6f2d1178eccddcb7f89e7d24ad15907a6
nemliProg <younesbuzz@gmail.com>
2022-12-16 20:36:11 +0100

    add findByEmail method to ClientRepository to get Client by email

--- bank-connect/src/main/java/ma/bankconnect/repository/ClientRepository.java
+++ bank-connect/src/main/java/ma/bankconnect/repository/ClientRepository.java
@@ -4,7 +4,10 @@ import ma.bankconnect.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
+    Optional<Client> findByEmail(String email);
 }
