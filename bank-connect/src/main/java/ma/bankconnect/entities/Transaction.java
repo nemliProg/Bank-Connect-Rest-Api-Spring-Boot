7f0003e080c0370bbc858a50265b14e69de64a15
nemliProg <younesbuzz@gmail.com>
2022-12-16 19:30:06 +0100

    update Transaction Entity again

--- bank-connect/src/main/java/ma/bankconnect/entities/Transaction.java
+++ bank-connect/src/main/java/ma/bankconnect/entities/Transaction.java
@@ -1,2 +1,32 @@
-package ma.bankconnect.entities;public class Transaction {
+package ma.bankconnect.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    private long id;
+
+    @ManyToOne
+    @JoinColumn( name = "sender")
+    private Account sender;
+
+    @ManyToOne
+    @JoinColumn( name = "receiver")
+    private Account receiver;
+
+    private double amount;
+
+    private LocalDateTime date;
 }
