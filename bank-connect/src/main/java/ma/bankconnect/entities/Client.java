a4ef638f3c655046f47533b676081d712da57529
nemliProg <younesbuzz@gmail.com>
2022-12-16 19:26:12 +0100

    update Client Entity

--- bank-connect/src/main/java/ma/bankconnect/entities/Client.java
+++ bank-connect/src/main/java/ma/bankconnect/entities/Client.java
@@ -2,11 +2,16 @@ package ma.bankconnect.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ma.bankconnect.entities.superclass.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -26,11 +31,15 @@ public class Client extends User {
     @Column( name = "phone_number" )
     String phoneNumber;
 
-    String adress;
+    String adresse;
 
     @Column( name = "code_sms" )
     String codeSms;
 
     @Column( name = "time_code_sms" )
-    String timeCodeSms;
+    LocalDateTime timeCodeSms;
+
+    @OneToMany(mappedBy = "client")
+    List<Account> accounts;
+
 }
