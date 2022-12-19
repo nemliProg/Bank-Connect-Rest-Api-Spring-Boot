7c64a05ec18a1bf44f6eb5754b858f32c826930a
nemliProg <younesbuzz@gmail.com>
2022-12-19 17:12:17 +0100

    add method returns transactions by account

--- bank-connect/src/main/java/ma/bankconnect/repository/TransactionRepository.java
+++ bank-connect/src/main/java/ma/bankconnect/repository/TransactionRepository.java
@@ -1,10 +1,15 @@
 package ma.bankconnect.repository;
 
+import ma.bankconnect.entities.Account;
 import ma.bankconnect.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findAllBySenderOrReceiver(Account account);
+
 
 }
