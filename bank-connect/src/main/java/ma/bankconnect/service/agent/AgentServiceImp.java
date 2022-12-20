e68945c13b21c174dd9e80ac9cbab20386669b01
nemliProg <younesbuzz@gmail.com>
2022-12-20 10:56:10 +0100

    add getAgentByEmail method to AgentServiceImp class

--- bank-connect/src/main/java/ma/bankconnect/service/agent/AgentServiceImp.java
+++ bank-connect/src/main/java/ma/bankconnect/service/agent/AgentServiceImp.java
@@ -1,13 +1,24 @@
 package ma.bankconnect.service.agent;
 
 import ma.bankconnect.entities.Agent;
+import ma.bankconnect.repository.AgentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Service
+@Transactional
 public class AgentServiceImp implements AgentService {
+
+    private AgentRepository agentRepository;
+
+    public AgentServiceImp(AgentRepository agentRepository) {
+        this.agentRepository = agentRepository;
+    }
 
     @Override
     public Optional<Agent> getAgentByEmail(String email) {
-        return Optional.empty();
+        return agentRepository.findByEmail(email);
     }
 }
