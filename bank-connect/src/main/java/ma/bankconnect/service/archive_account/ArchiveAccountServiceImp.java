122c58646fc4309c581b1db51b31d5eb9baf36fa
nemliProg <younesbuzz@gmail.com>
2022-12-20 11:05:26 +0100

    some modifications in ArchiveAccountServiceImp

--- bank-connect/src/main/java/ma/bankconnect/service/archive_account/ArchiveAccountServiceImp.java
+++ bank-connect/src/main/java/ma/bankconnect/service/archive_account/ArchiveAccountServiceImp.java
@@ -1,32 +1,57 @@
 package ma.bankconnect.service.archive_account;
 
+import ma.bankconnect.entities.Account;
 import ma.bankconnect.entities.ArchiveAccount;
 import ma.bankconnect.repository.AccountRepository;
+import ma.bankconnect.repository.ArchiveAccountRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class ArchiveAccountServiceImp implements ArchiveAccountService {
 
     private AccountRepository accountRepository;
 
+    private ArchiveAccountRepository archiveAccountRepository;
+
+    public ArchiveAccountServiceImp(AccountRepository accountRepository, ArchiveAccountRepository archiveAccountRepository) {
+        this.accountRepository = accountRepository;
+        this.archiveAccountRepository = archiveAccountRepository;
+    }
+
+
+
     @Override
     public ArchiveAccount archiveAccount(Long accountId, String reason) {
+        //  check if account is exist
+        Account account = accountRepository.findById(accountId).get();
+        if ( account == null){
+            return null;
+        }
+        //  archive account
         ArchiveAccount archiveAccount = new ArchiveAccount();
-        archiveAccount.setAccount(accountRepository.findById(accountId).get());
+        archiveAccount.setAccount(account);
         archiveAccount.setReason(reason);
         return archiveAccount;
     }
 
     @Override
     public boolean checkIfAccountIsArchived(Long accountId) {
-        return false;
+        return archiveAccountRepository.existsById(accountId);
     }
 
     @Override
     public ArchiveAccount getArchiveAccountByAccountId(Long accountId) {
-        return null;
+        return archiveAccountRepository.findById(accountId).get();
     }
 
     @Override
     public boolean unarchiveAccount(Long accountId) {
+        if (checkIfAccountIsArchived(accountId)){
+            archiveAccountRepository.deleteById(accountId);
+            return true;
+        }
         return false;
     }
 }
