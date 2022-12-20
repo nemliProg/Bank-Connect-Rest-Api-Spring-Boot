46e1577a60eb24cf8d7c2b9be86447e2204c5e17
nemliProg <younesbuzz@gmail.com>
2022-12-20 10:58:41 +0100

    add archiveClient method to ArchiveClientServiceImp class

--- bank-connect/src/main/java/ma/bankconnect/service/archive_client/ArchiveClientServiceImp.java
+++ bank-connect/src/main/java/ma/bankconnect/service/archive_client/ArchiveClientServiceImp.java
@@ -1,11 +1,38 @@
 package ma.bankconnect.service.archive_client;
 
 import ma.bankconnect.entities.ArchiveClient;
+import ma.bankconnect.entities.Client;
+import ma.bankconnect.repository.ArchiveClientRepository;
+import ma.bankconnect.repository.ClientRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class ArchiveClientServiceImp implements ArchiveClientService {
+
+    private ArchiveClientRepository archiveClientRepository;
+
+    private ClientRepository clientRepository;
+
+    public ArchiveClientServiceImp(ArchiveClientRepository archiveClientRepository, ClientRepository clientRepository) {
+        this.archiveClientRepository = archiveClientRepository;
+        this.clientRepository = clientRepository;
+    }
+
     @Override
-    public boolean archiveClient(Long accountId, String reason) {
-        return false;
+    public boolean archiveClient(Long clientId, String reason) {
+        //  check if client is exist
+        Client client = clientRepository.findById(clientId).get();
+        if (client == null){
+            return false;
+        }
+        //  archive client
+        ArchiveClient archiveClient = new ArchiveClient();
+        archiveClient.setClient(clientRepository.findById(clientId).get());
+        archiveClient.setReason(reason);
+        archiveClientRepository.save(archiveClient);
+        return true;
     }
 
     @Override
