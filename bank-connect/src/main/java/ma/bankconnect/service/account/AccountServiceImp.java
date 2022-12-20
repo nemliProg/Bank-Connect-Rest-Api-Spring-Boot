da52defd6a3742532f8bc06fa29ca6c3196d5a41
nemliProg <younesbuzz@gmail.com>
2022-12-20 07:40:06 +0100

    add transfer money method

--- bank-connect/src/main/java/ma/bankconnect/service/account/AccountServiceImp.java
+++ bank-connect/src/main/java/ma/bankconnect/service/account/AccountServiceImp.java
@@ -1,15 +1,42 @@
 package ma.bankconnect.service.account;
 
 import ma.bankconnect.entities.Account;
+import ma.bankconnect.entities.Transaction;
+import ma.bankconnect.repository.AccountRepository;
+import ma.bankconnect.service.transaction.TransactionServiceImp;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Service
+@Transactional
 public class AccountServiceImp implements AccountService{
 
+    private AccountRepository accountRepository;
+    private TransactionServiceImp transactionServiceImp;
+
+    public AccountServiceImp(AccountRepository accountRepository, TransactionServiceImp transactionServiceImp) {
+        this.accountRepository = accountRepository;
+        this.transactionServiceImp = transactionServiceImp;
+    }
+
     @Override
-    public boolean transferMoney(Account sender, Account receiver, double amount, LocalDateTime date) {
-        return false;
+    public boolean transferMoney(Account sender, Account receiver, double amount) {
+        // check if the sender has enough money
+        if (sender.getSolde() < amount){
+            return false;
+        }
+        // Transfer money
+        Transaction transaction = transactionServiceImp.transfer(sender,receiver,amount);
+        // check if the transaction is done
+        if (transaction == null){
+            return false;
+        }
+        // update the sender and receiver accounts and return true
+        accountRepository.save(sender);
+        accountRepository.save(receiver);
+        return true;
     }
 
     @Override
